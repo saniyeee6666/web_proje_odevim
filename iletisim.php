07eae0b9b419d071387370c2e0fe36c138c16721
Saniye Topal <topalsaniye3@gmail.com>
2026-05-08 23:07:49 +0300

    iletisim.php olusturuldu

---
+++ iletisim.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+
+    $ad       = $_POST['adi'];
+    $soyad    = $_POST['soyadi'];
+    $email    = $_POST['email'];
+    $tel      = $_POST['tel'];
+    $sehir    = $_POST['sehir'];
+    $cinsiyet = $_POST['cinsiyet'];
+    $mesaj    = $_POST['mesaj'];
+
+    echo "<h2>İletişim Formu Sonuçları</h2>";
+    echo "<hr>";
+    echo "<b>Ad Soyad:</b> " . $ad . " " . $soyad . "<br>";
+    echo "<b>E-posta:</b> " . $email . "<br>";
+    echo "<b>Telefon:</b> " . $tel . "<br>";
+    echo "<b>Şehir:</b> " . $sehir . "<br>";
+    echo "<b>Cinsiyet:</b> " . $cinsiyet . "<br>";
+    echo "<b>Mesaj:</b> " . $mesaj . "<br>";
+
+    echo "<br><a href='iletisim.html'>Geri Dön</a>";
+}
+?>
